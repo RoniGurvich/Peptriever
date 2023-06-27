9a4f86368137fa305c02fd93de150c52ff49b1b9
Roni Gurvich <14060729+RoniGurvich@users.noreply.github.com>
2023-06-27 19:09:39 +0300

    Annoy Cleanup (#12)
    * publishing to milvus by default
    
    * cleaning up annoy indexing residuals
    
    * documentation update
    
    ---------
    
    Co-authored-by: roni <roni@pc>

--- peptriever/indexing/config.py
+++ peptriever/indexing/config.py
@@ -4,9 +4,6 @@ from pathlib import Path
 
 @dataclass
 class IndexingConfig:
-    n_trees: int = 10
-    pep_threshold: int = 20
-
     hf_pdb_seq_repo: str = "ronig/pdb_sequences"
     hf_tokenizer_repo: str = "ronig/pdb_bpe_tokenizer_1024_mlm"
     max_length1: int = 30
@@ -16,13 +13,10 @@ class IndexingConfig:
     encoded_sequence_dims: int = 128
     distance_function: str = "euclidean"
 
-    min_indexed_entries: int = 5_000
-
-    local_indexes_path: Path = Path("/data/indexes/")
+    min_indexed_entries: int = 1_000
 
     hf_binding_dataset_repo: str = "ronig/protein_binding_sequences"
     hf_model_repo: str = "ronig/protein_biencoder"
-    hf_index_repo: str = "ronig/protein_index"
     hf_demo_space: str = "ronig/protein_binding_search"
 
     milvus_uri = "https://in03-ddab8e9a5a09fcc.api.gcp-us-west1.zillizcloud.com"
